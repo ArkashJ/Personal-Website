e4c81765d1931ca3e43d9de27e3a2a9d260562c5
Arkash Jain <arkjain@bu.edu>
2022-12-29 17:43:54 +0530

    .

--- components/Nav.js
+++ components/Nav.js
@@ -6,18 +6,18 @@ const Nav = () => {
         <div className='conatiner mx-auto flex flex-wrap items-center justify-between'>
             <h1 className='text-[#e2e8f0] text-2xl'>Arkash Jain's Portfolio</h1>
             <div className='hidden w-full md:block md:w-auto'>
-                <ul className='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+                <ul className='flex flex-col  p-4 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
                     <li className='text-white'>
-                        <Link  href='/'>Home</Link>
+                        <Link  href='/' className='text-lg'>Home</Link>
                     </li>
                     <li className='text-white'>
-                        <Link href='/Volunteering'>Volunteering</Link>
+                        <Link href='/Volunteering' className='text-lg'>Volunteering</Link>
                     </li>
                     <li className='text-white'>
-                        <Link href='/Projects'>Projects</Link>
+                        <Link href='/VC' className='text-lg'>Venture Capital</Link>
                     </li>
                     <li className='text-white'>
-                        <Link href='/VC'>Venture Capital</Link>
+                        <Link href='/Projects' className='text-lg'>Interests</Link>
                     </li>
                 </ul>
             </div>
