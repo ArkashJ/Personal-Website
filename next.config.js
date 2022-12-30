8b7624980f665fcef5956bb4baf795ba0d1acae6
Arkash Jain <arkjain@bu.edu>
2022-12-30 15:19:03 +0530

    .

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
