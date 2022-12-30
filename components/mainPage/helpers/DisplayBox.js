ab7edbd512f945e75c034f4339fd91065f98ae84
Arkash Jain <arkjain@bu.edu>
2022-12-30 15:35:44 +0530

    Versioning untracked files

--- components/mainPage/helpers/DisplayBox.js
+++ components/mainPage/helpers/DisplayBox.js
@@ -7,6 +7,7 @@ const DisplayBox = (props) => {
                     width       = {80}
                     height      = {80}
                     className   = "hover:scale-110 transform transition-transform pb-2"
+                    alt         = ""
                 />
                 <p className='text-xl font-mono text-gray-400'>{props.item.name}</p>
     </div>
