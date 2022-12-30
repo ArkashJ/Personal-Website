17be422ee20418aee7cf8644af37a70a7aecb980
Arkash Jain <arkjain@bu.edu>
2022-12-30 15:05:29 +0530

    done!

--- components/Nav.js
+++ components/Nav.js
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <nav className='bg-white border-gray-200 px-2 sm:px-4 py-2.5  dark:bg-gray-900 font-poppins'>
         <div className='conatiner mx-auto flex flex-wrap items-center justify-between'>
-            <h1 className='text-[#e2e8f0] text-2xl'>Arkash Jain's Portfolio</h1>
+            <h1 className='text-[#e2e8f0] text-2xl'> <Link  href='/'>Arkash Jain's Portfolio</Link></h1>
             <div className='hidden w-full md:block md:w-auto'>
                 <ul className='flex flex-col  p-4 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
                     <li className='text-white'>
@@ -15,9 +15,6 @@ const Nav = () => {
                     </li>
                     <li className='text-white'>
                         <Link href='/VC' className='text-lg'>Experience</Link>
-                    </li>
-                    <li className='text-white'>
-                        <Link href='/Projects' className='text-lg'>Interests</Link>
                     </li>
                 </ul>
             </div>
--- pages/Projects.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Projects = () => {
-  return (
-    <div className='w-full h-screen'>Interests</div>
-  )
-}
-
-export default Projects
